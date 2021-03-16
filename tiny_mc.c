0689dfc82b9a254823ca49678ac0a1854e11094c
Carlos Bederian <carlos.bederian@unc.edu.ar>
2021-03-15 23:58:32 -0300

    Clang-format

--- tiny_mc.c
+++ tiny_mc.c
@@ -8,10 +8,10 @@
 #include "params.h"
 #include "wtime.h"
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char t1[] = "Tiny Monte Carlo by Scott Prahl (http://omlc.ogi.edu)";
 char t2[] = "1 W Point Source Heating in Infinite Isotropic Scattering Medium";
@@ -31,46 +31,52 @@ float heat2[SHELLS];
  * Photon
  ***/
 
-static void photon(void) {
-	unsigned int shell;
-	float x, y, z, u, v, w, weight;
-	float xi1, xi2, t;
-	const float albedo = MU_S / (MU_S + MU_A);
-	const float shells_per_mfp = 1e4/MICRONS_PER_SHELL/(MU_A+MU_S);
+static void photon(void)
+{
+    unsigned int shell;
+    float x, y, z, u, v, w, weight;
+    float xi1, xi2, t;
+    const float albedo = MU_S / (MU_S + MU_A);
+    const float shells_per_mfp = 1e4 / MICRONS_PER_SHELL / (MU_A + MU_S);
 
-	x = 0.0f; y = 0.0f; z = 0.0f; /* launch */
-	u = 0.0f; v = 0.0f; w = 1.0f;
-	weight = 1.0f;
+    x = 0.0f;
+    y = 0.0f;
+    z = 0.0f; /* launch */
+    u = 0.0f;
+    v = 0.0f;
+    w = 1.0f;
+    weight = 1.0f;
 
-	for (;;) {
-		t = -logf(rand()/(float)RAND_MAX); /* move */
-		x += t * u;
-		y += t * v;
-		z += t * w;
+    for (;;) {
+        t = -logf(rand() / (float)RAND_MAX); /* move */
+        x += t * u;
+        y += t * v;
+        z += t * w;
 
-		shell=sqrtf(x*x+y*y+z*z)*shells_per_mfp; /* absorb */
-		if (shell > SHELLS-1) {
-			shell = SHELLS-1;
-		}
-		heat[shell] += (1.0f-albedo)*weight;
-		heat2[shell] += (1.0f-albedo)*(1.0f-albedo)*weight*weight;  /* add up squares */
-		weight *= albedo;
+        shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp; /* absorb */
+        if (shell > SHELLS - 1) {
+            shell = SHELLS - 1;
+        }
+        heat[shell] += (1.0f - albedo) * weight;
+        heat2[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
+        weight *= albedo;
 
-		/* New direction, rejection method */
-		do {
-			xi1 = 2.0f*rand()/(float)RAND_MAX - 1.0f;
-			xi2 = 2.0f*rand()/(float)RAND_MAX - 1.0f;
-			t = xi1*xi1+xi2*xi2;
-		} while (1.0f<t);
-		u = 2.0f * t - 1.0f;
-		v = xi1 * sqrtf((1.0f-u*u)/t);
-		w = xi2 * sqrtf((1.0f-u*u)/t);
+        /* New direction, rejection method */
+        do {
+            xi1 = 2.0f * rand() / (float)RAND_MAX - 1.0f;
+            xi2 = 2.0f * rand() / (float)RAND_MAX - 1.0f;
+            t = xi1 * xi1 + xi2 * xi2;
+        } while (1.0f < t);
+        u = 2.0f * t - 1.0f;
+        v = xi1 * sqrtf((1.0f - u * u) / t);
+        w = xi2 * sqrtf((1.0f - u * u) / t);
 
-		if (weight < 0.001f) { /* roulette */
-			if (rand()/(float)RAND_MAX > 0.1f) break;
-			weight /= 0.1f;
-		}
-	}
+        if (weight < 0.001f) { /* roulette */
+            if (rand() / (float)RAND_MAX > 0.1f)
+                break;
+            weight /= 0.1f;
+        }
+    }
 }
 
 
@@ -80,41 +86,41 @@ static void photon(void) {
 
 int main(void)
 {
-	double start = 0.0, end = 0.0, elapsed = 0.0;
-	float t = 0.0f;
-	unsigned int i = 0;
+    double start = 0.0, end = 0.0, elapsed = 0.0;
+    float t = 0.0f;
+    unsigned int i = 0;
 
-	// heading
-	printf("# %s\n# %s\n# %s\n", t1, t2, t3);
-	printf("# Scattering = %8.3f/cm\n", MU_S);
-	printf("# Absorption = %8.3f/cm\n", MU_A);
-	printf("# Photons    = %8d\n#\n", PHOTONS);
+    // heading
+    printf("# %s\n# %s\n# %s\n", t1, t2, t3);
+    printf("# Scattering = %8.3f/cm\n", MU_S);
+    printf("# Absorption = %8.3f/cm\n", MU_A);
+    printf("# Photons    = %8d\n#\n", PHOTONS);
 
-	// configure RNG
-	srand(SEED);
-	// start timer
-	start = wtime();
-	// simulation
-	for (i=0; i<PHOTONS; ++i) {
-		photon();
-	}
-	// stop timer
-	end = wtime();
-	assert(start<=end);
-	elapsed = end-start;
+    // configure RNG
+    srand(SEED);
+    // start timer
+    start = wtime();
+    // simulation
+    for (i = 0; i < PHOTONS; ++i) {
+        photon();
+    }
+    // stop timer
+    end = wtime();
+    assert(start <= end);
+    elapsed = end - start;
 
-	printf("# %lf seconds\n", elapsed);
-	printf("# %lf K photons per second\n", PHOTONS/elapsed * MILLISEC);
+    printf("# %lf seconds\n", elapsed);
+    printf("# %lf K photons per second\n", 1e-3 * PHOTONS / elapsed);
 
-	printf("# Radius\tHeat\n");
-	printf("# [microns]\t[W/cm^3]\tError\n");
-	t = 4.0f*M_PI*powf(MICRONS_PER_SHELL, 3.0f)*PHOTONS/1e12;
-	for (i=0; i<SHELLS-1; ++i) {
-		printf("%6.0f\t%12.5f\t%12.5f\n", i*(float)MICRONS_PER_SHELL,
-				heat[i]/t/(i*i+i+1.0/3.0),
-				sqrt(heat2[i]-heat[i]*heat[i]/PHOTONS)/t/(i*i+i+1.0f/3.0f));
-	}
-	printf("# extra\t%12.5f\n", heat[SHELLS-1]/PHOTONS);
+    printf("# Radius\tHeat\n");
+    printf("# [microns]\t[W/cm^3]\tError\n");
+    t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * PHOTONS / 1e12;
+    for (i = 0; i < SHELLS - 1; ++i) {
+        printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
+               heat[i] / t / (i * i + i + 1.0 / 3.0),
+               sqrt(heat2[i] - heat[i] * heat[i] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
+    }
+    printf("# extra\t%12.5f\n", heat[SHELLS - 1] / PHOTONS);
 
-	return 0;
+    return 0;
 }
